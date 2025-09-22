55c6f434d289bd49fd3a5cce7d3ea372ecf5b109
zuoyi34 <GitHub@example.com>
2025-09-22 22:09:03 +0800

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import path from 'path'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import path from 'path';
 
 export default defineConfig({
   plugins: [vue()],
@@ -8,10 +8,5 @@ export default defineConfig({
     alias: {
       '@': path.resolve(__dirname, './src')
     }
-  },
-  server: {
-    host: true,
-    port: 5173
-  },
-  base: '/'  // ✅ Railway 是部署在根路径
-})
+  }
+});
